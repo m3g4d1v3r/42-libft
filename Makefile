9fb9da4ddd6fdc38207100cc1a21c3cfd717c50a
m3g4d1v3r <marlon365x@protonmail.ch>
2022-04-05 12:21:55 -0300

    Function 06: memset

--- Makefile
+++ Makefile
@@ -4,7 +4,8 @@ SRCS	= ft_isalpha.c \
 		  ft_isalnum.c \
 		  ft_isascii.c \
 		  ft_isprint.c \
-		  ft_strlen.c
+		  ft_strlen.c \
+		  ft_memset.c
 INCD	= ./
 OBJS	= ${SRCS:.c=.o}
 NAME	= libft.a
--- ft_memset.c
+++ ft_memset.c
@@ -1,29 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: msubtil- <msubtil-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/04/04 21:16:43 by msubtil-          #+#    #+#             */
-/*   Updated: 2022/04/05 12:13:37 by msubtil-         ###   ########.fr       */
+/*   Created: 2022/04/05 12:01:47 by msubtil-          #+#    #+#             */
+/*   Updated: 2022/04/05 12:21:17 by msubtil-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#include <libft.h>
 
-# include <stddef.h>
-# define FT_ASCII_CONTROL_LOWER 0
-# define FT_ASCII_CONTROL_UPPER 31
-# define FT_ASCII_DELETE 127
+void	*ft_memset(void *s, int c, size_t n)
+{
+	size_t	byte_offset;
 
-int		ft_isalpha(int c);
-int		ft_isdigit(int c);
-int		ft_isalnum(int c);
-int		ft_isascii(int c);
-int		ft_isprint(int c);
-size_t	ft_strlen(const char *s);
-void	*ft_memset(void *s, int c, size_t n);
-
-#endif
+	byte_offset = 0;
+	while (byte_offset < n)
+	{
+		*((unsigned char *)(s + byte_offset)) = (unsigned char) c;
+		byte_offset++;
+	}
+	return (s);
+}
