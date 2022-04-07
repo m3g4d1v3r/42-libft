e4f1ddf202dae714506e76b8146643074ebc6a7c
m3g4d1v3r <marlon365x@protonmail.ch>
2022-04-06 21:59:29 -0300

    Function 16: memchr

--- Makefile
+++ Makefile
@@ -15,7 +15,8 @@ SRCS	= ft_isalpha.c \
 		  ft_tolower.c \
 		  ft_strchr.c \
 		  ft_strrchr.c \
-		  ft_strncmp.c
+		  ft_strncmp.c \
+		  ft_memchr.c
 INCD	= ./
 OBJS	= ${SRCS:.c=.o}
 NAME	= libft.a
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: msubtil- <msubtil-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/04 21:16:43 by msubtil-          #+#    #+#             */
-/*   Updated: 2022/04/06 20:09:21 by msubtil-         ###   ########.fr       */
+/*   Updated: 2022/04/06 21:50:25 by msubtil-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,5 +37,6 @@ int		ft_tolower(int c);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
+void	*ft_memchr(const void *s, int c, size_t n);
 
 #endif
