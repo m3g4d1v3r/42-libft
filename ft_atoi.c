72b4c157faaa5c9ad06bba332a9e1d6064075e08
m3g4d1v3r <marlon365x@protonmail.ch>
2022-04-07 22:53:40 -0300

    Function 19: atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -1,20 +1,35 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_isdigit.c                                       :+:      :+:    :+:   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: msubtil- <msubtil-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/04/04 22:04:37 by msubtil-          #+#    #+#             */
-/*   Updated: 2022/04/07 22:12:35 by msubtil-         ###   ########.fr       */
+/*   Created: 2022/04/07 22:01:16 by msubtil-          #+#    #+#             */
+/*   Updated: 2022/04/07 22:50:38 by msubtil-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <libft.h>
 
-int	ft_isdigit(int c)
+int	ft_atoi(const char *nptr)
 {
-	if (c >= '0' && c <= '9')
-		return (1);
-	return (0);
+	int	acc;
+	int	new_value;
+
+	acc = 0;
+	while (*nptr != '\0')
+	{
+		if (ft_isdigit(*nptr))
+		{
+			new_value = 10 * acc + (*nptr - '0');
+			if (new_value <= acc)
+				return (-1);
+			acc = new_value;
+		}
+		else if (*nptr != ' ')
+			return (0);
+		nptr++;
+	}
+	return (acc);
 }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: msubtil- <msubtil-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/04 21:16:43 by msubtil-          #+#    #+#             */
-/*   Updated: 2022/04/07 21:13:15 by msubtil-         ###   ########.fr       */
+/*   Updated: 2022/04/07 22:01:40 by msubtil-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,5 +40,6 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 char	*ft_strnstr(const char *big, const char *little, size_t len);
+int		ft_atoi(const char *nptr);
 
 #endif
