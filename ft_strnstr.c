78f45d121b36aa6c5175917d20a89f5f0247349f
m3g4d1v3r <marlon365x@protonmail.ch>
2022-04-08 00:43:08 -0300

    Refactor strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,34 +6,70 @@
 /*   By: msubtil- <msubtil-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/06 22:13:36 by msubtil-          #+#    #+#             */
-/*   Updated: 2022/04/07 21:58:35 by msubtil-         ###   ########.fr       */
+/*   Updated: 2022/04/10 13:21:50 by msubtil-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <libft.h>
+#define BASE 0x10
+#define BASE_SHIFTER 4
+
+size_t	ft_bin_base_msb(size_t base_shifter, size_t exp)
+{
+	return (1 << (base_shifter * (exp)));
+}
+
+size_t	ft_hash(const char *str, size_t str_len)
+{
+	size_t	hash;
+	size_t	i;
+
+	hash = 0;
+	i = 0;
+	while (i < str_len)
+	{
+		hash = hash * 0x10 + *str++;
+		i++;
+	}
+	return (hash);
+}
+
+size_t	ft_rolling_hash(char newc, char oldc, size_t pow, size_t lhash)
+{
+	return (0x10 * (lhash - (oldc * pow)) + newc);
+}
+
+short	ft_check_if_matches(const char *str1, const char *str2, size_t len)
+{
+	while (len--)
+	{
+		if (*str1 != *str2)
+			return (0);
+	}
+	return (1);
+}
 
 char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
+	size_t	b_hash;
 	size_t	l_len;
-	size_t	acc;
+	size_t	l_hash;
+	size_t	pow;
 
 	l_len = ft_strlen(little);
-	acc = 0;
-	while (*big != '\0' && len--)
+	if (ft_strlen(big) < l_len)
+		return ((char *) 0);
+	b_hash = ft_hash(big, l_len);
+	l_hash = ft_hash(little, l_len);
+	pow = ft_bin_base_msb(BASE_SHIFTER, (l_len - 1));
+	len -= (l_len - 1);
+	while (*(big + l_len - 1) != '\0' && len--)
 	{
-		if (*big == *little)
-		{
-			acc++;
-			little++;
-		}
-		else if (!(acc == 1 && (*big == *(little - 1))) && acc > 0)
-		{
-			little -= acc;
-			acc = 0;
-		}
+		if (b_hash == l_hash)
+			if (ft_check_if_matches(big, little, l_len))
+				return ((char *) big);
+		b_hash = ft_rolling_hash(*(big + l_len), *(big), pow, b_hash);
 		big++;
-		if (acc == l_len)
-			return ((char *)(big - l_len));
 	}
 	return ((char *) 0);
 }
