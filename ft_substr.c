86c3d7fa36cbd7e6540bc319cdc827f7a9dd7fa3
m3g4d1v3r <marlon365x@protonmail.ch>
2022-04-12 16:17:10 -0300

    Refactor substr

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: msubtil- <msubtil-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/12 15:31:45 by msubtil-          #+#    #+#             */
-/*   Updated: 2022/04/12 15:42:36 by msubtil-         ###   ########.fr       */
+/*   Updated: 2022/04/12 16:13:04 by msubtil-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	size_t	index;
 
 	substr = (char *) malloc(sizeof(char) * len);
+	if (substr == (void *) 0)
+		return (substr);
 	index = 0;
 	while (index < len)
 	{
