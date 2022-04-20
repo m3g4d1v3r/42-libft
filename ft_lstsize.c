ce043e5926f37b6f27161fb14adf24b193a1b4d9
m3g4d1v3r <marlon365x@protonmail.ch>
2022-04-20 20:25:27 -0300

    Refactor lstsize

--- ft_lstsize.c
+++ ft_lstsize.c
@@ -6,7 +6,7 @@
 /*   By: msubtil- <msubtil-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/20 19:57:25 by msubtil-          #+#    #+#             */
-/*   Updated: 2022/04/20 20:04:48 by msubtil-         ###   ########.fr       */
+/*   Updated: 2022/04/20 20:12:12 by msubtil-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int	ft_lstsize(t_list *lst)
 {
-	size_t size;
+	size_t	size;
 
 	size = 0;
 	while (lst != NULLPTR)
