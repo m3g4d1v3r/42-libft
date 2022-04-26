b67909e9c833c11949f6b3bd5eb34edbd0d22986
m3g4d1v3r <marlon365x@protonmail.ch>
2022-04-25 21:54:11 -0300

    Tripouille tests refactoring: ft_split (part 2)

--- ft_split.c
+++ ft_split.c
@@ -6,19 +6,39 @@
 /*   By: msubtil- <msubtil-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/12 15:31:45 by msubtil-          #+#    #+#             */
-/*   Updated: 2022/04/20 18:41:23 by msubtil-         ###   ########.fr       */
+/*   Updated: 2022/04/25 21:53:15 by msubtil-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <libft.h>
+
+int	ft_isspace_split(const char chr)
+{
+	if (chr == ' ')
+		return (1);
+	else if (chr == '\t')
+		return (1);
+	else if (chr == '\n')
+		return (1);
+	else if (chr == '\v')
+		return (1);
+	else if (chr == '\f')
+		return (1);
+	else if (chr == '\r')
+		return (1);
+	return (0);
+}
 
 size_t	ft_strlen_split(char const *s, char c)
 {
 	size_t	len;
 
 	len = 0;
-	while (*s++ != c)
+	while (*s != c && *s != '\0')
+	{
 		len++;
+		s++;
+	}
 	return (len);
 }
 
@@ -79,11 +99,18 @@ void	ft_fill_table(char **dst, char const *s, char c)
 char	**ft_split(char const *s, char c)
 {
 	char	**table;
-	size_t	table_size;
+	char	delimiter_set[1];
+	size_t	substrings_nb;
 
-	table_size = ft_get_substrs_nb(s, c) + 1;
-	table = (char **) malloc(sizeof(char *) * table_size);
-	ft_fill_table(table, --s, c);
-	table[table_size - 1] = NULLPTR;
+	substrings_nb = ft_get_substrs_nb(s, c);
+	table = (char **) malloc(sizeof(char *) * (substrings_nb + 1));
+	if (substrings_nb > 1)
+		ft_fill_table(table, --s, c);
+	else if (substrings_nb == 1)
+	{
+		delimiter_set[0] = c;
+		table[0] = ft_strtrim((char *) s, delimiter_set);
+	}
+	table[substrings_nb] = NULLPTR;
 	return (table);
 }
