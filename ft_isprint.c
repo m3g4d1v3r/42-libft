2077470f97d7ae6fb17f67349d9027b3a495961d
m3g4d1v3r <marlon365x@protonmail.ch>
2022-05-11 21:55:31 -0300

    Fix ft_isprint edge cases implementation

--- ft_isprint.c
+++ ft_isprint.c
@@ -6,7 +6,7 @@
 /*   By: msubtil- <msubtil-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/04 23:54:00 by msubtil-          #+#    #+#             */
-/*   Updated: 2022/04/05 00:12:01 by msubtil-         ###   ########.fr       */
+/*   Updated: 2022/05/11 21:53:53 by msubtil-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ int	ft_isprint(int c)
 	if (c >= FT_ASCII_CONTROL_LOWER && c <= FT_ASCII_CONTROL_UPPER)
 		return (0);
 	if (c == FT_ASCII_DELETE)
+		return (0);
+	if (c > FT_ASCII_DELETE)
 		return (0);
 	return (1);
 }
